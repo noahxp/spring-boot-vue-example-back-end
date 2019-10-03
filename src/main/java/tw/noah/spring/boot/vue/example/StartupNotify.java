a063d95db64850a5daa025d24837aff35e26c276
Noah Liu <noahxp@gmail.com>
2019-10-03 16:43:35 +0800

    fix timezone

--- src/main/java/tw/noah/spring/boot/vue/example/StartupNotify.java
+++ src/main/java/tw/noah/spring/boot/vue/example/StartupNotify.java
@@ -1,5 +1,9 @@
 package tw.noah.spring.boot.vue.example;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -11,6 +15,9 @@ public class StartupNotify implements ApplicationListener<ContextRefreshedEvent>
 
   @Override
   public void onApplicationEvent(ContextRefreshedEvent event) {
+    NumberFormat format = new DecimalFormat("###,###,###");
+    log.info("Max memory=" + format.format(Runtime.getRuntime().maxMemory()));
+    log.info("now is :" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     log.info("Application startup success...");
   }
 }
--- src/main/java/tw/noah/spring/boot/vue/example/controller/LabsController.java
+++ src/main/java/tw/noah/spring/boot/vue/example/controller/LabsController.java
@@ -1,5 +1,6 @@
 package tw.noah.spring.boot.vue.example.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,28 +19,33 @@ public class LabsController {
   private int cnt = 0;
 
   @GetMapping(value = "/int")
-  public int getBooksCount() {
+  public int getInt() {
     return cnt++;
   }
 
   @GetMapping(value = "/string")
-  public String getBooksString() {
+  public String getString() {
     return "Hi,Books";
   }
 
   @GetMapping(value = "/bool")
-  public boolean getBooksBoolean() {
+  public boolean getBoolean() {
     return true;
   }
 
   @GetMapping(value = "/date")
-  public Date getBooksDate() {
+  public Date getDate() {
     return new Date();
+  }
+
+  @GetMapping(value = "/date/str")
+  public String getDateStr(){
+    return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
   }
 
 
   @GetMapping(value = "/error")
-  public Object getBooksError() {
+  public Object getError() {
     if (true) {
       int c = 0 / 0;
     }
@@ -54,5 +60,10 @@ public class LabsController {
   @GetMapping(value = "/number/sub/{x}/{y}")
   public long cacheSub(@PathVariable(name="x") int x,@PathVariable(name="y") int y){
     return cacheDemoService.subtraction(x,y);
+  }
+
+  @GetMapping(value = "/jvm/opts")
+  public String getJvmOpts(){
+    return System.getProperty("user.timezone");
   }
 }
